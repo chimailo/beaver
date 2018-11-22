8042d483af4c1053befeb595ca6745a24999217f
chima <kollign@yahoo.com>
2018-11-22 13:49:40 +0100

    Created Factories, Migrations, Models, Resources

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Model\Product;
 use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'detail' => $faker->paragraph,
+        'price' => $faker->numberBetween($min = 10, $max = 9000),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween($min = 4, $max = 30),
     ];
 });
